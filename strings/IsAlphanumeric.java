e04830fc5bc877389bc89a6ebd144b06f00ea66e
Rizwan <rizwanmushtaq15@gmail.com>
2025-09-29 09:22:55 +0200

    initial commit

---
+++ strings/IsAlphanumeric.java
@@ -0,0 +1,24 @@
+class IsAphanumeric {
+  public static void main(String[] args) {
+    System.out.println("IsAphanumeric Problem");
+    System.out.println(IsAphanumeric('#'));
+    System.out.println(IsAphanumeric('Z'));
+    System.out.println(IsAphanumeric('9'));
+  }
+
+  private static boolean IsAphanumeric(char c) {
+    if ((int) c >= (int) 'a' && (int) c <= (int) 'z') {
+      return true;
+    }
+
+    if ((int) c >= (int) 'A' && (int) c <= (int) 'Z') {
+      return true;
+    }
+
+    if ((int) c >= (int) '0' && (int) c <= (int) '9') {
+      return true;
+    }
+
+    return false;
+  }
+}
