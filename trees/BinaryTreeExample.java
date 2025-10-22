f8a40b804df0abddd3756f5ab0d09ca2bff79711
Rizwan <rizwanmushtaq15@gmail.com>
2025-10-22 21:41:49 +0200

    dsa: added BinaryTreeExample

---
+++ trees/BinaryTreeExample.java
@@ -0,0 +1,30 @@
+package trees;
+
+public class BinaryTreeExample {
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+    root.left = new TreeNode(5);
+    root.right = new TreeNode(20);
+    root.left.left = new TreeNode(-5);
+    root.left.right = new TreeNode(0);
+
+    System.out.println(root.left.left.isLeaf());
+    System.out.println(root.left.isLeaf());
+  }
+}
+
+class TreeNode {
+  int data;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  boolean isLeaf() {
+    return this.left == null && this.right == null;
+  }
+}
