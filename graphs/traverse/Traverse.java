801c03875f9f8508dcd576bd4563d40e5f542c31
Rizwan <rizwanmushtaq15@gmail.com>
2025-11-06 12:56:21 +0100

    dsa: graphs updated Traverse.java with bfs

--- graphs/traverse/Traverse.java
+++ graphs/traverse/Traverse.java
@@ -1,21 +1,42 @@
 package graphs.traverse;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Traverse {
   public static void main(String[] args) {
+    Traverse traverse = new Traverse();
     List<List<Integer>> graph =
         List.of(List.of(1, 3), List.of(0, 3), List.of(3), List.of(0, 1, 2, 4), List.of(3));
-
-    List<Integer> result = new Traverse().dfs(graph, 0);
-    System.out.println("graph: " + graph + " dfs: " + result);
+    System.out.println("graph: " + graph + " dfs: " + traverse.dfs(graph, 0));
+    System.out.println("graph: " + graph + " bfs: " + traverse.bfs(graph, 0));
 
     List<List<Integer>> graph2 =
         List.of(List.of(1, 2), List.of(0, 3, 4), List.of(0, 3), List.of(1, 2, 4), List.of(1, 3));
+    System.out.println("graph: " + graph2 + " bfs: " + traverse.bfs(graph2, 1));
+    System.out.println("graph: " + graph2 + " dfs: " + traverse.dfs(graph2, 1));
+  }
 
-    List<Integer> result2 = new Traverse().dfs(graph2, 1);
-    System.out.println("graph: " + graph2 + " dfs: " + result2);
+  private List<Integer> bfs(List<List<Integer>> graph, Integer v) {
+    if (v == null) return new ArrayList<>();
+    if (v < 0 || v >= graph.size()) return new ArrayList<>();
+    Queue<Integer> queue = new LinkedList<>();
+
+    List<Integer> visited = new ArrayList<>();
+    queue.add(v);
+    visited.add(v);
+    while (!queue.isEmpty()) {
+      int node = queue.remove();
+      for (int nbr : graph.get(node)) {
+        if (!visited.contains(nbr)) {
+          visited.add(nbr);
+          queue.add(nbr);
+        }
+      }
+    }
+    return visited;
   }
 
   private List<Integer> dfs(List<List<Integer>> graph, Integer v) {
