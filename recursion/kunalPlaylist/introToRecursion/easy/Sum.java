af8fb59f62471dff0c3df7371cbc47ffb095cf63
Rizwan <rizwanmushtaq15@gmail.com>
2025-11-28 21:02:07 +0100

    recursion: Sum.java

---
+++ recursion/kunalPlaylist/introToRecursion/easy/Sum.java
@@ -0,0 +1,13 @@
+package recursion.kunalPlaylist.introToRecursion.easy;
+
+public class Sum {
+  public static void main(String[] args) {
+    System.out.println(sum(5));
+  }
+
+  static int sum(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return n + sum(n - 1);
+  }
+}
