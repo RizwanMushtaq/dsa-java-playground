b67a5c70c74a65a79c90d2d95232762067067ce5
Rizwan <rizwanmushtaq15@gmail.com>
2025-11-28 20:43:39 +0100

    recursion: updated Nto1.java

--- recursion/kunalPlaylist/introToRecursion/easy/Nto1.java
+++ recursion/kunalPlaylist/introToRecursion/easy/Nto1.java
@@ -3,8 +3,7 @@ package recursion.kunalPlaylist.introToRecursion.easy;
 public class Nto1 {
 
   public static void main(String[] args) {
-    nTo1(5);
-    nTo1Reverse(5);
+    nTo1Both(5);
   }
 
   static void nTo1(int n) {
@@ -16,6 +15,13 @@ public class Nto1 {
   static void nTo1Reverse(int n) {
     if (n == 0) return;
     nTo1Reverse(n - 1);
+    System.out.println(n);
+  }
+
+  static void nTo1Both(int n) {
+    if (n == 0) return;
+    System.out.println(n);
+    nTo1Both(n - 1);
     System.out.println(n);
   }
 }
