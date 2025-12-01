426d6eb7a4efd2e60d82133d47676ff9b236198a
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-01 09:03:41 +0100

    recursion: NoOfZeros.java

---
+++ recursion/kunalPlaylist/introToRecursion/easy/NoOfZeros.java
@@ -0,0 +1,21 @@
+package recursion.kunalPlaylist.introToRecursion.easy;
+
+public class NoOfZeros {
+  public static void main(String[] args) {
+    System.out.println(solve(4506505));
+    System.out.println(solve(5));
+  }
+
+  static int solve(int n) {
+    int count = 0;
+    return helper(n, count);
+  }
+
+  static int helper(int n, int count) {
+    if (n % 10 == n) return count;
+    if (n % 10 == 0) {
+      count++;
+    }
+    return helper(n / 10, count);
+  }
+}
