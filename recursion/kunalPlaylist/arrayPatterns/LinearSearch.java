d753b094ed3a624583e05f45321a90a279e87046
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-01 11:01:59 +0100

    recursion: array patterns - LinearSearch.java

--- recursion/kunalPlaylist/arrayPatterns/LinearSearch.java
+++ recursion/kunalPlaylist/arrayPatterns/LinearSearch.java
@@ -10,7 +10,7 @@ public class LinearSearch {
   }
 
   static int helper(int[] arr, int target, int index) {
-    if (index > arr.length - 1) return -1;
+    if (index == arr.length) return -1;
     if (arr[index] == target) return index;
     return helper(arr, target, index + 1);
   }
