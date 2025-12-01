2168c77b3fbde7e2998714ee486a9f99235cf09d
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-01 12:06:06 +0100

    recursion: array patterns - TargetOccurrences.java

--- recursion/kunalPlaylist/arrayPatterns/TargetOccurrences.java
+++ recursion/kunalPlaylist/arrayPatterns/TargetOccurrences.java
@@ -9,14 +9,28 @@ public class TargetOccurrences {
     System.out.println(solve(new int[] {8, 5, 4, 5}, 40));
   }
 
-  private static List<Integer> solve(int[] arr, int target) {
-    List<Integer> result = new ArrayList<>();
-    return helper(arr, target, 0, result);
+  private static ArrayList<Integer> solve(int[] arr, int target) {
+    ArrayList<Integer> result = new ArrayList<>();
+    //    return helper(arr, target, 0, result);
+
+    return helperWithoutResultArray(arr, target, 0);
   }
 
   private static List<Integer> helper(int[] arr, int target, int index, List<Integer> result) {
     if (index == arr.length) return result;
     if (arr[index] == target) result.add(index);
     return helper(arr, target, index + 1, result);
+  }
+
+  // When we dont want to pass result array in argument and will create new
+  // array with every function call.
+  private static ArrayList<Integer> helperWithoutResultArray(int[] arr, int target, int index) {
+    ArrayList<Integer> list = new ArrayList<>();
+    if (index == arr.length) return list;
+    // this will contain only answer for that specific function call.
+    if (arr[index] == target) list.add(index);
+    ArrayList<Integer> ansFromBelowCalls = helperWithoutResultArray(arr, target, index + 1);
+    list.addAll(ansFromBelowCalls);
+    return list;
   }
 }
