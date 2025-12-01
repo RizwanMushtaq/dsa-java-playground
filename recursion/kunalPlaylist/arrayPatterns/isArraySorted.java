696bde044b42fbd320ec5986d0b42ea867792384
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-01 10:40:21 +0100

    recursion: array patterns - isArraySorted.java

---
+++ recursion/kunalPlaylist/arrayPatterns/isArraySorted.java
@@ -0,0 +1,17 @@
+package recursion.kunalPlaylist.arrayPatterns;
+
+public class isArraySorted {
+  public static void main(String[] args) {
+    System.out.println(solve(new int[] {5, 6, 10}));
+  }
+
+  static boolean solve(int[] arr) {
+    return helper(arr, 0);
+  }
+
+  static boolean helper(int[] arr, int index) {
+    if (index == arr.length - 1) return true;
+    if (arr[index] > arr[index + 1]) return false;
+    return helper(arr, index + 1);
+  }
+}
