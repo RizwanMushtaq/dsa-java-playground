2b854cecc5072ca707c3203127d84f1ce76358a8
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-01 11:21:29 +0100

    recursion: array patterns - TargetOccurrences.java

---
+++ recursion/kunalPlaylist/arrayPatterns/TargetOccurrences.java
@@ -0,0 +1,20 @@
+package recursion.kunalPlaylist.arrayPatterns;
+
+import java.util.*;
+
+public class TargetOccurrences {
+  public static void main(String[] args) {
+    System.out.println(solve(new int[] {8, 5, 4, 5}, 5));
+  }
+
+  private static List<Integer> solve(int[] arr, int target) {
+    List<Integer> result = new ArrayList<>();
+    return helper(arr, target, 0, result);
+  }
+
+  private static List<Integer> helper(int[] arr, int target, int index, List<Integer> result) {
+    if (index == arr.length) return result;
+    if (arr[index] == target) result.add(index);
+    return helper(arr, target, index + 1, result);
+  }
+}
