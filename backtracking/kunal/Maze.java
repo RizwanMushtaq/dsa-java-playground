544f77abab2d2aca8925fccd862dbf559eaeea6c
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-08 22:00:10 +0100

    backtracking - Maze - updated Maze.java

--- backtracking/kunal/Maze.java
+++ backtracking/kunal/Maze.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 
 public class Maze {
   public static void main(String[] args) {
-
-    System.out.println(pathsList("", 3, 3));
+    boolean[][] grid =
+        new boolean[][] {
+          {true, true, true},
+          {true, false, true},
+          {true, true, true},
+        };
+    pathsRestrictions("", grid, 2, 2);
+    //    printPaths("", 3, 3);
   }
 
   private static int solve(int r, int c) {
@@ -33,7 +39,7 @@ public class Maze {
   }
 
   // return List of all the paths from top left cell such as (3,3) to top bottom
-  // cell such as (0,0)
+  // cell such as (0,0) But you can only move right or down
   private static ArrayList<String> pathsList(String processed, int r, int c) {
     if (r == 1 && c == 1) {
       ArrayList<String> list = new ArrayList<>();
@@ -49,5 +55,41 @@ public class Maze {
       result.addAll(pathsList(processed.concat("R"), r, c - 1));
     }
     return result;
+  }
+
+  // return List of all the paths from top left cell such as (3,3) to top bottom
+  // cell such as (0,0) But you can only right or down or diagonal
+  private static ArrayList<String> pathsListDiagonal(String processed, int r, int c) {
+    if (r == 1 && c == 1) {
+      ArrayList<String> list = new ArrayList<>();
+      list.add(processed);
+      return list;
+    }
+
+    ArrayList<String> result = new ArrayList<>();
+    if (r > 1) {
+      result.addAll(pathsListDiagonal(processed.concat("V"), r - 1, c));
+    }
+    if (r > 1 && c > 1) {
+      result.addAll(pathsListDiagonal(processed.concat("D"), r - 1, c - 1));
+    }
+    if (c > 1) {
+      result.addAll(pathsListDiagonal(processed.concat("H"), r, c - 1));
+    }
+    return result;
+  }
+
+  // print all the paths from top left cell such as (3,3) to top bottom
+  // cell such as (0,0) But you can only right or down, You will also get
+  // Boolean Matrix and based on true condition on a cell, you can move to
+  // that cell otherwise there is obstacle
+  private static void pathsRestrictions(String processed, boolean[][] grid, int r, int c) {
+    if (r == 0 && c == 0) {
+      System.out.println(processed);
+      return;
+    }
+    if (grid[r][c] == false) return;
+    if (r > 0) pathsRestrictions(processed.concat("D"), grid, r - 1, c);
+    if (c > 0) pathsRestrictions(processed.concat("R"), grid, r, c - 1);
   }
 }
