d80c0a1f961cf2d644cb2757d16fc971dd32ad67
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-09 12:25:17 +0100

    backtracking - Maze - updated Maze.java

--- backtracking/kunal/Maze.java
+++ backtracking/kunal/Maze.java
@@ -1,6 +1,7 @@
 package backtracking.kunal;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Maze {
   public static void main(String[] args) {
@@ -9,7 +10,8 @@ public class Maze {
       {true, true, true},
       {true, true, true},
     };
-    printAllPathsBacktrack("", grid, 0, 0);
+    int[][] path = new int[grid.length][grid[0].length];
+    printAllPathsWithLevelsBacktrack("", grid, 0, 0, path, 1);
     //    printPaths("", 3, 3);
   }
 
@@ -109,5 +111,34 @@ public class Maze {
     // So before function gets removed, also remove the changes that were made
     // by that function
     grid[r][c] = true;
+  }
+
+  /*
+  Now you nned to print path like DDRR as well as path inside matrix
+   */
+  private static void printAllPathsWithLevelsBacktrack(
+      String processed, boolean[][] grid, int r, int c, int[][] path, int step) {
+    if (r == grid.length - 1 && c == grid[0].length - 1) {
+      path[r][c] = step;
+      System.out.println(processed);
+      for (int[] arr : path) {
+        System.out.println(Arrays.toString(arr));
+      }
+      path[r][c] = 0;
+      return;
+    }
+    if (!grid[r][c]) return;
+    grid[r][c] = false; // I am considering this path
+    path[r][c] = step;
+    if (r < grid.length - 1)
+      printAllPathsWithLevelsBacktrack(processed.concat("D"), grid, r + 1, c, path, step + 1);
+    if (c < grid[0].length - 1)
+      printAllPathsWithLevelsBacktrack(processed.concat("R"), grid, r, c + 1, path, step + 1);
+    if (c > 0) printAllPathsWithLevelsBacktrack(processed + "L", grid, r, c - 1, path, step + 1);
+    if (r > 0) printAllPathsWithLevelsBacktrack(processed + "U", grid, r - 1, c, path, step + 1);
+    // So before function gets removed, also remove the changes that were made
+    // by that function
+    grid[r][c] = true;
+    path[r][c] = 0;
   }
 }
