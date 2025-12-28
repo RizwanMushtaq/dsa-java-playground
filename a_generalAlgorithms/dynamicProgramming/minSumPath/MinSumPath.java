5a5ca1c1c8d05a8e655d9e73ba4ef46b7f639cd2
Rizwan <rizwanmushtaq15@gmail.com>
2025-12-28 13:45:05 +0100

    feat - DP - MinSumPath.java

---
+++ a_generalAlgorithms/dynamicProgramming/minSumPath/MinSumPath.java
@@ -0,0 +1,38 @@
+package a_generalAlgorithms.dynamicProgramming.minSumPath;
+
+public class MinSumPath {
+  public static void main(String[] args) {
+    int[][] grid = {
+      {1, 4, 3},
+      {2, 7, 6},
+      {5, 8, 9},
+    };
+    MinSumPathSol minSumPathSol = new MinSumPathSol();
+    System.out.println("minSumPathSol = " + minSumPathSol.solve(grid));
+  }
+}
+
+class MinSumPathSol {
+  private int rows, columns;
+
+  public int solve(int[][] grid) {
+    rows = grid.length;
+    columns = grid[0].length;
+    int[][] memo = new int[rows][columns];
+    return visit(0, 0, grid, memo);
+  }
+
+  private int visit(int r, int c, int[][] grid, int[][] memo) {
+    if (r == rows - 1 && c == columns - 1) return grid[r][c];
+    if (memo[r][c] != 0) return memo[r][c];
+    else if (r == rows - 1) {
+      memo[r][c] = grid[r][c] + visit(r, c + 1, grid, memo);
+      return memo[r][c];
+    } else if (c == columns - 1) {
+      return memo[r][c] = grid[r][c] + visit(r + 1, c, grid, memo);
+    } else {
+      return memo[r][c] =
+          grid[r][c] + Math.min(visit(r + 1, c, grid, memo), visit(r, c + 1, grid, memo));
+    }
+  }
+}
