06424e477e49d1a21a827d4a1b5575a8ec60189f
Rizwan <rizwanmushtaq15@gmail.com>
2026-01-10 15:09:56 +0100

    feat - updated AdjacencyListValidation

--- graphs/adjacencyListTopic/adjancencyListValidation/AdjacencyListValidation.java
+++ graphs/adjacencyListTopic/adjancencyListValidation/AdjacencyListValidation.java
@@ -9,6 +9,7 @@ public class AdjacencyListValidation {
   }
 
   boolean solve(int[][] graph) {
+
     if (graph == null) return false;
     int V = graph.length;
     for (int node = 0; node < V; node++) {
@@ -32,7 +33,7 @@ public class AdjacencyListValidation {
         }
       }
     }
-    // If node1 appears in graph[node1], then node2 also appear in graph[node1]
+    // If node1 appears in graph[node2], then node2 also appear in graph[node1]
     return edges.isEmpty();
   }
 }
