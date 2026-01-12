8bd62af4ed153b4064132d5764e81652027560b2
Rizwan <rizwanmushtaq15@gmail.com>
2026-01-12 10:56:03 +0100

    feat - Trees&Graphs - ctci-ch4-problem4.2

---
+++ graphs/ctci/minimalTree/MinimalTree.java
@@ -0,0 +1,40 @@
+package graphs.ctci.minimalTree;
+
+public class MinimalTree {
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7};
+    Node n = new MinimalTree().solve(arr);
+    printInOrderTraversal(n);
+  }
+
+  private static void printInOrderTraversal(Node n) {
+    if (n == null) return;
+    printInOrderTraversal(n.left);
+    System.out.println(n.id + " ");
+    printInOrderTraversal(n.right);
+  }
+
+  private Node solve(int[] arr) {
+    return visit(arr, 0, arr.length - 1);
+  }
+
+  private Node visit(int[] arr, int start, int end) {
+    if (start > end) return null;
+    int mid = start + (end - start) / 2;
+    Node n = new Node(arr[mid]);
+    n.left = visit(arr, start, mid - 1);
+    n.right = visit(arr, mid + 1, end);
+    return n;
+  }
+
+  static class Node {
+    int id;
+    Node left, right;
+
+    public Node(int id) {
+      this.id = id;
+      left = null;
+      right = null;
+    }
+  }
+}
