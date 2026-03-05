540be837c15b695a4f1b94adc13e3602807d9e03
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-05 06:57:06 +0100

    feature: ctci - custom hashMap Implementation In-Progress

--- ctci/ch1_arraysAndStrings/customImplOfHashTable/CustomHashMapImp.java
+++ ctci/ch1_arraysAndStrings/customImplOfHashTable/CustomHashMapImp.java
@@ -26,4 +26,39 @@ package ctci.ch1_arraysAndStrings.customImplOfHashTable;
  *
  * <p>So how does it actually works
  */
-public class MyHashTable {}
+class CustomHashMapImp {
+  public static void main() {
+    System.out.println("Custom Hash Map");
+  }
+}
+
+class MyHashMap {
+  private final Entity[] entities;
+
+  public MyHashMap() {
+    entities = new Entity[100];
+  }
+
+  public void put(String key, String value) {
+    int hash = Math.abs(key.hashCode() % entities.length);
+    entities[hash] = new Entity(key, value); // overriding
+  }
+
+  public String get(String key) {
+    int hash = Math.abs(key.hashCode() % entities.length);
+    if (entities[hash] != null && entities[hash].key.equals(key)) {
+      return entities[hash].value;
+    }
+    return null;
+  }
+}
+
+class Entity {
+  String key;
+  String value;
+
+  public Entity(String key, String value) {
+    this.key = key;
+    this.value = value;
+  }
+}
