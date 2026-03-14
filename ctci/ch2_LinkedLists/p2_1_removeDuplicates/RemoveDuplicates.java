9998b75e38d870fa5162840d5de1511fbe4f8234
Rizwan <rizwanmushtaq15@gmail.com>
2026-03-14 12:36:51 +0100

    feature: ctci - problem 2.1 RemoveDuplicates

--- ctci/ch2_LinkedLists/p2_1_removeDuplicates/RemoveDuplicates.java
+++ ctci/ch2_LinkedLists/p2_1_removeDuplicates/RemoveDuplicates.java
@@ -1,34 +1,17 @@
 package ctci.ch2_LinkedLists.p2_1_removeDuplicates;
 
 import java.util.HashSet;
+import java.util.Objects;
 
 class RemoveDuplicates {
   public static void main() {
-    System.out.println("remove duplicates");
-    Node head = new Node(5);
-    head.next = new Node(5);
-    head.next.next = new Node(3);
-    head.next.next.next = new Node(5);
-    head.next.next.next.next = new Node(2);
-    head.next.next.next.next.next = new Node(5);
-
-    RemoveDuplicates rm = new RemoveDuplicates();
-    rm.print(head);
-    rm.solveWithoutBuffer(head);
-    rm.print(head);
-
-    Node head2 = new Node(5);
-    head2.next = new Node(5);
-    head2.next.next = new Node(3);
-    head2.next.next.next = new Node(5);
-    head2.next.next.next.next = new Node(2);
-    head2.next.next.next.next.next = new Node(5);
-    rm.print(head2);
-    rm.solveWithBuffer(head2);
-    rm.print(head2);
+    Test test = new Test();
+    test.test();
   }
 
-  void solveWithBuffer(Node head) {
+  Node solveWithBuffer(Node head) {
+    /*
+    -> First implementation by me
     if (head == null) return;
     Node n = head;
     HashSet<Integer> buffer = new HashSet<>();
@@ -41,10 +24,26 @@ class RemoveDuplicates {
       buffer.add(n.next.data);
       n = n.next;
     }
+    */
+    // Alternative for Duplicate Removal (more standard "prev" pattern)
+    Node curr = head;
+    Node prev = null;
+    HashSet<Integer> buffer = new HashSet<>();
+    while (curr != null) {
+      if (buffer.contains(curr.data)) {
+        prev.next = curr.next;
+      } else {
+        buffer.add(curr.data);
+        prev = curr;
+      }
+      curr = curr.next;
+    }
+    return head;
   }
 
-  void solveWithoutBuffer(Node head) {
-    for (Node current = head; current != null; current = current.next) {
+  Node solveWithoutBuffer(Node head) {
+    Node current = head;
+    while (current != null) {
       Node runner = current;
       while (runner.next != null) {
         if (current.data == runner.next.data) {
@@ -53,26 +52,89 @@ class RemoveDuplicates {
           runner = runner.next;
         }
       }
+      current = current.next;
+    }
+    return head;
+  }
+}
+
+class Node {
+  int data;
+  Node next;
+
+  Node(int data) {
+    this.data = data;
+    next = null;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Node node = (Node) o;
+    return data == node.data && Objects.equals(next, node.next);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(data, next);
+  }
+
+  @Override
+  public String toString() {
+    return "Node{" + "next=" + next + ", data=" + data + '}';
+  }
+}
+
+class TestData {
+  Node head, result;
+
+  TestData(Node head, Node result) {
+    this.head = head;
+    this.result = result;
+  }
+
+  @Override
+  public String toString() {
+    return "TestData{" + "head=" + head + ", result=" + result + '}';
+  }
+}
+
+class Test {
+  void test() {
+    Node head = new Node(5);
+    head.next = new Node(5);
+    head.next.next = new Node(3);
+    head.next.next.next = new Node(3);
+
+    Node result = new Node(5);
+    result.next = new Node(3);
+
+    Node head2 = new Node(10);
+    Node result2 = new Node(10);
+
+    TestData[] testData = {new TestData(head, result), new TestData(head2, result2)};
+    RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+    System.out.println("solution with buffer");
+    for (TestData item : testData) {
+      Node output = removeDuplicates.solveWithBuffer(item.head);
+      if (areLLIdentical(output, item.result)) {
+        System.out.println(".");
+      } else {
+        throw new RuntimeException("test failed" + item);
+      }
+    }
+    System.out.println("solution without buffer");
+    for (TestData item : testData) {
+      Node output = removeDuplicates.solveWithoutBuffer(item.head);
+      if (areLLIdentical(output, item.result)) {
+        System.out.println(".");
+      } else {
+        throw new RuntimeException("test failed" + item);
+      }
     }
   }
 
-  void print(Node head) {
-    if (head == null) return;
-    Node n = head;
-    while (n.next != null) {
-      System.out.print(n.data + "->");
-      n = n.next;
-    }
-    System.out.println(n.data);
-  }
-
-  static class Node {
-    int data;
-    Node next;
-
-    Node(int data) {
-      this.data = data;
-      next = null;
-    }
+  boolean areLLIdentical(Node a, Node b) {
+    return a.equals(b);
   }
 }
